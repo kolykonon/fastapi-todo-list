8080c30da332324a78f47d8c32790a34fd168f85
kolykonon <@kononikhinnikolay@gmail.com>
2026-03-13 16:07:20 +0300

    Исправил импорт из файла на импорт из модуля

--- models/task.py
+++ models/task.py
@@ -1,7 +1,7 @@
 from sqlalchemy import String, Text
 from mixins import TimeStampMixin, IDMixin
 from sqlalchemy.orm import Mapped, mapped_column
-from base import Base
+from models import Base
 import enum
 
 
