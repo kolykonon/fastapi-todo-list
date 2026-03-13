6137d4b60e9b3442658bcc58f6a61d9569022c2d
kolykonon <@kononikhinnikolay@gmail.com>
2026-03-13 18:45:07 +0300

    Добавил первоначальную модель пользователя

---
+++ models/user.py
@@ -0,0 +1,14 @@
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from typing import TYPE_CHECKING, List
+from models import Base
+from mixins import IDMixin, TimeStampMixin
+from pydantic import EmailStr
+
+if TYPE_CHECKING:  # Чтобы избежать циклического импорта
+    from models import Task
+
+
+class User(Base, IDMixin, TimeStampMixin):
+    email: Mapped[EmailStr] = mapped_column(unique=True, nullable=False)
+    hashed_password: Mapped[str] = mapped_column(nullable=False)
+    tasks: Mapped[List["Task"]] = relationship(back_populates="user")
