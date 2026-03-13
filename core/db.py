7433a2e2b517acf61653cde647b4f55268ba7e41
kolykonon <@kononikhinnikolay@gmail.com>
2026-03-13 15:09:28 +0300

    Реализовал класс для удобной работы с сессиями

--- core/db.py
+++ core/db.py
@@ -1,11 +1,25 @@
+from typing import Annotated, AsyncGenerator
+from fastapi import Depends
 from config import db_settings
 from pydantic_settings import BaseSettings
-from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 
 
 class DBHelper:
+    """Класс для удобной работы с сессиями бд"""
+
     def __init__(self):
         self.engine = create_async_engine(db_settings.get_db_url)
         self.session_factory = async_sessionmaker(
             bind=self.engine, autoflush=False, expire_on_commit=False
         )
+        self.session_dep = Annotated[AsyncSession, Depends(self.get_session)]
+
+    async def get_session(
+        self,
+    ) -> AsyncGenerator[AsyncSession]:  # Функция для проброса сессии
+        async with self.session_factory.begin() as session:
+            yield session
+
+
+db_helper = DBHelper()
