cbc9b453f2be579a3a039e600bcfbd861dd4ae08
kolykonon <@kononikhinnikolay@gmail.com>
2026-03-13 15:37:18 +0300

    Добавил класс статусов задачи и класс задачи в бд

---
+++ models/task.py
@@ -0,0 +1,26 @@
+from sqlalchemy import String, Text
+
+from mixins import TimeStampMixin, IDMixin
+from sqlalchemy.orm import Mapped, mapped_column
+from base import Base
+import enum
+
+
+class TaskStatus(str, enum.Enum):
+    """Класс статусов задач"""
+
+    COMPLETED = "Completed"
+    IN_PROGRESS = "In progress"
+    CANCELED = "canceled"
+
+
+class Task(Base, IDMixin, TimeStampMixin):
+    """Класс, описывающий задачу в базе данных"""
+
+    title: Mapped[str] = mapped_column(String(100), nullable=False)
+    description: Mapped[str] = mapped_column(Text(500), nullable=True, default="")
+    status: Mapped[str] = mapped_column(
+        String(20),
+        default=TaskStatus.IN_PROGRESS,
+        server_default=TaskStatus.IN_PROGRESS,
+    )
