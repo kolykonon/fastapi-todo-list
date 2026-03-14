b32eea1ead03450b50fb6f1ee89eb77fbdbc8a56
kolykonon <@kononikhinnikolay@gmail.com>
2026-03-14 13:25:01 +0300

    Добавил auth_router в __all__

--- api/__init__.py
+++ api/__init__.py
@@ -1,3 +1,4 @@
-from api.task_view import router as tasks_router
+from api.tasks import router as tasks_router
+from api.auth import router as auth_router
 
-__all__ = "tasks_router"
+__all__ = ("tasks_router", "auth_router")
