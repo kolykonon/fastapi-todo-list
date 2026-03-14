b54b0bf720f207d287a25fd07d864e30cea51aca
kolykonon <@kononikhinnikolay@gmail.com>
2026-03-14 14:22:07 +0300

    Добавил схему токена в __all__

--- schemas/__init__.py
+++ schemas/__init__.py
@@ -1,4 +1,10 @@
 from schemas.task_schemas import TaskAddSchema, TaskSchema
 from schemas.user_schemas import UserSchema, CreateUserSchema
 
-__all__ = ("TaskAddSchema", "TaskSchema", "UserSchema", "CreateUserSchema")
+__all__ = (
+    "TaskAddSchema",
+    "TaskSchema",
+    "UserSchema",
+    "CreateUserSchema",
+    "TokenSchema",
+)
