ef1d7d1dd44ae7f636ff727b38429e4487b7a521
kolykonon <@kononikhinnikolay@gmail.com>
2026-03-14 14:22:54 +0300

    Добавил импорт

--- schemas/__init__.py
+++ schemas/__init__.py
@@ -1,5 +1,6 @@
 from schemas.task_schemas import TaskAddSchema, TaskSchema
 from schemas.user_schemas import UserSchema, CreateUserSchema
+from schemas.token_schemas import TokenSchema
 
 __all__ = (
     "TaskAddSchema",
