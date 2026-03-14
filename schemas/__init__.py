49af163ca0a87b23ab08c658ff3a69792e7fd265
kolykonon <@kononikhinnikolay@gmail.com>
2026-03-14 13:26:17 +0300

    Добавил схемы пользователей в __all__

--- schemas/__init__.py
+++ schemas/__init__.py
@@ -1,3 +1,4 @@
 from schemas.task_schemas import TaskAddSchema, TaskSchema
+from schemas.user_schemas import UserSchema, CreateUserSchema
 
-__all__ = ("TaskAddSchema", "TaskSchema")
+__all__ = ("TaskAddSchema", "TaskSchema", "UserSchema", "CreateUserSchema")
