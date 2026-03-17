bedde617d617b5c4d395aa55aa88b0e9fbe4af51
kolykonon <@kononikhinnikolay@gmail.com>
2026-03-17 13:12:25 +0300

    Сделал исключение существования универсальным для всех моделей

--- app/api/v1/exceptions.py
+++ app/api/v1/exceptions.py
@@ -10,11 +10,12 @@ class TaskNotFoundException(HTTPException):
         super().__init__(status_code, detail)
 
 
-class TaskAlreadyExistsException(HTTPException):
+class AlreadyExistsException(HTTPException):
     def __init__(
-        self,
-        status_code=status.HTTP_400_BAD_REQUEST,
-        detail="Задача с таким названием уже существует",
-        headers=None,
+        self, substance: str, status_code=status.HTTP_400_BAD_REQUEST, headers=None
     ):
-        super().__init__(status_code, detail, headers)
+        super().__init__(
+            status_code=status_code,
+            headers=headers,
+            detail=f"{substance} уже существует!",
+        )
